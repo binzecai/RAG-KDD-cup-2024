27cefeabadd0b76b49eb9c9e4e7ca350b5bc2502
zhenggr <john.zhenggenrong@outlook.com>
2024-04-22 22:29:24 +0800

    update rag_model 0422

--- starter-kit/models/rag_model.py
+++ starter-kit/models/rag_model.py
@@ -14,16 +14,19 @@ class RAGModel:
         self.embedding_model = BGEpeftEmbedding(model_path=self.embedding_path)
 
     def generate_answer(self, query, search_results) -> str:
-        docs = extract_web_chunks(search_results)
-        db = FAISS.from_documents(docs, self.embedding_model)
-        db.save_local(folder_path='./vector', index_name='index_256')
-        search_docs = db.similarity_search(query, k=5)
-        references = ""
-        for doc in search_docs:
-            references += "<DOC>\n" + doc.page_content + "\n</DOC>\n"
-        references = " ".join(
-            references.split()[:500]
-        )
-        prediction = self.model.predict(references, query)
-        prediction = prediction.strip()
+        try:
+            docs = extract_web_chunks(search_results)
+            db = FAISS.from_documents(docs, self.embedding_model)
+            db.save_local(folder_path='./vector', index_name='index_256')
+            search_docs = db.similarity_search(query, k=5)
+            references = ""
+            for doc in search_docs:
+                references += "<DOC>\n" + doc.page_content + "\n</DOC>\n"
+            references = " ".join(
+                references.split()[:500]
+            )
+            prediction = self.model.predict(references, query)
+            prediction = prediction.strip()
+        except:
+            prediction = "I don't know"
         return prediction
